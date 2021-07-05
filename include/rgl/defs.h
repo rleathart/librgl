33338d8aaece9eb3b9f631171cfe295937db4ea5
rleathart <56587304+rleathart@users.noreply.github.com>
2021-07-05 12:43:40 +0100

    fixup! add sleep_ms

---
+++ include/rgl/defs.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/**
+ * This file is just definitions that I think are handy or make C less of a pain
+ * to use.
+ */
+
+#include <stdint.h>
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef u8 byte;
